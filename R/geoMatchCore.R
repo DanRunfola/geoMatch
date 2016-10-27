97bc133a2a130a7a9a0961d73eb7de49fe077143
geogdan <dan@danrunfola.com>
2016-10-27 10:10:31 -0400

    MatchIt compatability testing

--- R/geoMatchCore.R
+++ R/geoMatchCore.R
@@ -131,7 +131,7 @@ geoMatch.Core <- function (..., outcome.variable,outcome.suffix="_adjusted"){
   m.res <- do.call("matchit", a)
 
   #Fix the data call to account for the do.match and (for now!) lack of substitute.
-  m.res$call$data <- as.list(substitute(list(...)))[-1L]$data
+  m.res$call$data <- as.list(substitute(list(...)))[-1L]$data@data
   
   #Update the source data with the new column.
   #This is to play nice with MatchIt's match.data() call.
